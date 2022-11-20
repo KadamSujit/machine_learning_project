98669c61e192a8ce58e94391a00e22d22e66809f
KadamSujit <sujitpkadam@gmail.com>
2022-11-20 19:52:27 +0530

    training pipeline configuration

--- housing/config/configuration.py
+++ housing/config/configuration.py
@@ -1,12 +1,23 @@
 from housing.entity.config_entity import DataIngestionConfig,DataValidationConfig,DataTransformationConfig,ModelTrainerConfig,ModelEvaluationConfig,ModelPusherConfig,TrainingPipelineConfig
 # or from housing.entity.config_entity import *
+from housing.util.util import read_yaml_file
+from housing.logger import logging
+import sys,os
+from housing.constant import *
+from housing.exception import HousingException
 
 
 
 class Configuration:
     
-    def __init__(self) -> None:
-        pass
+    def __init__(self,
+        config_file_path:str=CONFIG_FILE_PATH,
+        current_time_stamp:str=CURRENT_TIME_STAMP
+        ) -> None:
+        self.config_info=read_yaml_file(file_path=config_file_path)
+        self.training_pipeline_config = self.get_training_pipeline_config()
+        self.time_stamp=current_time_stamp
+
 
     def get_data_ingestion_config(self) -> DataIngestionConfig: #information after -> symbol is return type of this function
         pass
@@ -27,6 +38,20 @@ class Configuration:
         pass
 
     def get_training_pipeline_config(self) -> TrainingPipelineConfig:
-        pass
+        try:
+           training_pipeline_config = self.config_info[TRAINING_PIPELINE_CONFIG_KEY]
+           artifact_dir = os.path.join(ROOT_DIR,
+           training_pipeline_config[TRAINING_PIPELINE_NAME_KEY],
+           training_pipeline_config[TRAINING_PIPELINE_ARTIFACT_DIR_KEY]
+           ) #artifact_dir is just a path combining root dir,housing dir and artifact dir
+
+           #creating named tuple
+           training_pipeline_config = TrainingPipelineConfig(artifact_dir=artifact_dir)
+           logging.info(f"Training pipeline config: {training_pipeline_config}")
+           return training_pipeline_config
+
+        except Exception as e:
+            raise HousingException(e,sys) from e
+
 
 
