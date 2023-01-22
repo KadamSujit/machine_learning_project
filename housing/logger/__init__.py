097be714c82332f12ed40aa428596d1b06c21c70
KadamSujit <sujitpkadam@gmail.com>
2023-01-22 11:44:50 +0530

    project successfully completed

--- housing/logger/__init__.py
+++ housing/logger/__init__.py
@@ -1,23 +1,24 @@
 import logging
 from datetime import datetime
 import os
+import pandas as pd
+from housing.constant import get_current_time_stamp 
 
-LOG_DIR = "housing_logs" #directory name
+LOG_DIR="logs" #directory name
 
-CURRENT_TIME_STAMP = f"{datetime.now().strftime('%Y-%m-%d_%H-%M-%S')}" #capturing current time stamp
+def get_log_file_name():
+    return f"log_{get_current_time_stamp()}.log"
 
-LOG_FILE_NAME = f'log_{CURRENT_TIME_STAMP}.log' #log file name
-
+LOG_FILE_NAME=get_log_file_name() #log file name
 os.makedirs(LOG_DIR,exist_ok=True) #creating directory, also checks if it is already present(if yes then it continues with the same).
-
 LOG_FILE_PATH = os.path.join(LOG_DIR,LOG_FILE_NAME) #getting log file path
 
 #writing log file at log file location with a good format
 logging.basicConfig(filename=LOG_FILE_PATH,
 filemode="w",
 format='[%(asctime)s]^;%(levelname)s^;%(lineno)d^;%(filename)s^;%(funcName)s()^;%(message)s',
-level=logging.INFO)
-
+level=logging.INFO
+)
 
 def get_log_dataframe(file_path):
     data=[]
@@ -32,3 +33,5 @@ def get_log_dataframe(file_path):
     log_df["log_message"] = log_df['Time stamp'].astype(str) +":$"+ log_df["message"]
 
     return log_df[["log_message"]]
+
+
